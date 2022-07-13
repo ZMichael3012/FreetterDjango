90ddfe63dac682b91c50ad9e767bbdabd17a1cc4
ZMichael3012 <michael.zaplatin@yandex.ru>
2022-07-13 22:31:34 +0700

    Changed DB config

--- Freetter/settings.py
+++ Freetter/settings.py
@@ -85,8 +85,19 @@ WSGI_APPLICATION = 'Freetter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': "Freetter",
+
+        'USER': 'postgres',
+
+        'PASSWORD': '12345678',
+
+        'HOST': "localhost",
+
+        'PORT': "5432",
+
     }
 }
 
